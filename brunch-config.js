63f1023ec4c6d2c77737ad5a1aa61eed3aa1faae
Alex Salomon <alexsalomon.th@gmail.com>
2018-05-10 12:52:57 +0100

    Configure Brunch.io to output files to root so GitHub pages can display the website

--- brunch-config.js
+++ brunch-config.js
@@ -1,14 +1,17 @@
 // See http://brunch.io for documentation.
-exports.files = {
-  javascripts: {
-    joinTo: {
-      'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
-      'app.js': /^app/
-    }
-  },
-  stylesheets: {joinTo: 'app.css'}
-};
 
-exports.plugins = {
-  babel: {presets: ['latest']}
+exports.config = {
+  paths: { public: './' },
+  files: {
+      javascripts: {
+          joinTo: {
+              'vendor.js': /^(?!app)/, // Files that are not in `app` dir.
+              'app.js': /^app/
+          },
+      },
+      stylesheets: {joinTo: 'app.css'},
+  },
+  plugins: {
+      babel: {presets: ['latest']},
+  },
 };
