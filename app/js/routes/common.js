12328dfe37c1eca558ad72bd4d524b5d4577a462
Alex Salomon <alexsalomon.th@gmail.com>
2018-06-02 13:01:57 +0100

    Animate burguer menu icon to x-close icon using css

--- app/js/routes/common.js
+++ app/js/routes/common.js
@@ -1,7 +1,17 @@
 
 export default {
     init() {
-        // JavaScript to be fired on all pages
+        const navbarBurguer = document.querySelector('.navbar-burger');
+        navbarBurguer.addEventListener('click', onClickToggleNavbarBurguerIconAnimation);
+
+        function onClickToggleNavbarBurguerIconAnimation() {
+            const navbarBurguerIcon = document.querySelector('.navbar-burger__icon');
+            if(navbarBurguerIcon.classList.contains('open')) {
+                navbarBurguerIcon.classList.remove('open');
+            } else {
+                navbarBurguerIcon.classList.add('open');
+            }
+        }
     },
     finalize() {
         // JavaScript to be fired on all pages, after page specific JS is fired
